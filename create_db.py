c1f1446abe0afb047b8a59142747bd62797e477e
malibaturhan <baturhan95@gmail.com>
2025-07-29 10:44:32 +0300

    Db creation

--- create_db.py
+++ create_db.py
@@ -1,41 +1,61 @@
 import sqlite3
 
-conn = sqlite3.connect("data.db")
-cursor = conn.cursor()
+def create_db():
+    conn = sqlite3.connect("data.db")
+    cursor = conn.cursor()
 
-cursor.execute("PRAGMA foreign_keys = ON;")
+    cursor.execute("PRAGMA foreign_keys = ON;")
 
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users(
-               id INTEGER PRIMARY KEY AUTOINCREMENT,
-               username TEXT NOT NULL UNIQUE,
-               email TEXT NOT NULL UNIQUE,
-               password_hash TEXT NOT NULL,
-               created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-               is_active INTEGER NOT NULL DEFAULT = 1
-               );
-""")
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL UNIQUE,
+                email TEXT NOT NULL UNIQUE,
+                password_hash TEXT NOT NULL,
+                created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                is_active INTEGER NOT NULL DEFAULT = 1
+                );
+    """)
 
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS images(
-        id INTEGER PRIMARY KEY NOT NULL,
-        filename TEXT NOT NULL,
-        original_filename TEXT,
-        title TEXT,
-        uploaded_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        deleted_at DATETIME,
-        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS images(
+            id INTEGER PRIMARY KEY NOT NULL,
+            filename TEXT NOT NULL,
+            original_filename TEXT,
+            title TEXT,
+            uploaded_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            deleted_at DATETIME,
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+            );
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS image_reactions(
+            user_id INTEGER NOT NULL,
+            image_id INTEGER NOT NULL,
+            reaction INTEGER NOT NULL CHECK (reaction IN (-1, 1)),
+            reacted_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (user_id, image_id),
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+            FOREIGN KEY (image_id) REFERENCES images(id) ON DELETE CASCADE
         );
-""")
+    """)
 
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS image_reactions(
-        user_id INTEGER NOT NULL,
-        image_id INTEGER NOT NULL,
-        reaction INTEGER NOT NULL CHECK (reaction IN (-1, 1)),
-        reacted_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        PRIMARY KEY (user_id, image_id),
-        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-        FOREIGN KEY (image_id) REFERENCES images(id) ON DELETE CASCADE
-    );
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS image_views(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   image_id INTEGER NOT NULL,
+                   user_id INTEGER, -- everyone can trigger a view
+                    viewed_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                   ip_hash TEXT,
+                   FOREING KEY (image_id) REFERENCES images(id) ON DELETE CASCADE,
+                   FOREING KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+
+                   );
 """)
+    
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    create_db()
